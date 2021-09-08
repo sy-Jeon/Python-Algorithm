e0d2e6a863786d6fb9239885728b8b7fc6d3008a
Sangyeol Jeon <ccccccccci@naver.com>
2021-09-08 23:26:13 +0900

    operation12

---
+++ codeupBasic100/codeupBasic44.py
@@ -0,0 +1,16 @@
+n1, n2 = input().split()
+n1, n2 = int(n1), int(n2)
+
+addtion = n1 + n2
+subtraction = n1 - n2
+multiplication = n1 * n2
+floor_division = n1 // n2
+modulus = n1 % n2
+division = n1 / n2
+
+print(addtion)
+print(subtraction)
+print(multiplication)
+print(floor_division)
+print(modulus)
+print(format(division, "0.2f"))
