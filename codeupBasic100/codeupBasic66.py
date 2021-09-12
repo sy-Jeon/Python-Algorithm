f1c61302a4b4d20be6d3c13a9fbf946353ff1966
Sangyeol Jeon <ccccccccci@naver.com>
2021-09-12 23:50:10 +0900

    even odd2

---
+++ codeupBasic100/codeupBasic66.py
@@ -0,0 +1,17 @@
+n1, n2, n3 = input().split()
+n1, n2, n3 = int(n1), int(n2), int(n3)
+
+if n1 % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+if n2 % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+if n3 % 2 == 0:
+    print("even")
+else:
+    print("odd")
