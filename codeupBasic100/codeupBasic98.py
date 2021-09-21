ea8808841914d4a12e904773d07016f85bda004f
Sangyeol Jeon <ccccccccci@naver.com>
2021-09-21 12:15:56 +0900

    list7

--- codeupBasic100/codeupBasic98.py
+++ codeupBasic100/codeupBasic98.py
@@ -6,33 +6,37 @@
 
 feed_x, feed_y = 0, 0
 # 미로 생성
-mazemap = []
-for i in range(10):
-    mazemap.append([])
+m = []
+for i in range(12):
+    m.append([])
     # 미로 입력받음
-    mazemap_input = input().split()
-    for j in range(10):
-        mazemap[i].append(int(mazemap_input[j]))
-        # 먹이 위치 찾기
-        if int(mazemap_input[j]) == 2:
-            feed_x, feed_y = i, j
+    for j in range(12):
+        m[i].append(0)
 
-x, y = 1, 1
+for i in range(10):
+    m_input = input().split()
+    for j in range(10):
+        m[i+1][j+1] = int(m_input[j])
+        # 먹이 위치 찾기
+        if int(m_input[j]) == 2:
+            feed_x, feed_y = i+1, j+1
+
+x, y = 2, 2
 while x != feed_x  or y != feed_y:
-    mazemap[x][y] = 9
+    m[x][y] = 9
     # 움직이기
-    if mazemap[x][y+1] != 1:
+    if m[x][y+1] != 1:
         y += 1
     else:
         x += 1
 
-    if x < 9 and y < 9:
-        mazemap[x][y] = 9
+    if x < 10 and y < 10:
+        m[x][y] = 9
     else:
         break
 
 # 결과 출력
-for i in range(10):
-    for j in range(10):
-        print(mazemap[i][j], end=" ")
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(m[i][j], end=" ")
     print()
