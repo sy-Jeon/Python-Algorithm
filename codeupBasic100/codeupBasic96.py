6bf5ff7d73b4b161a0737c101bcef64a36ba21d9
Sangyeol Jeon <ccccccccci@naver.com>
2021-09-22 22:51:01 +0900

    list5

--- codeupBasic100/codeupBasic96.py
+++ codeupBasic100/codeupBasic96.py
@@ -1,19 +1,19 @@
-# 바둑판 생성
+# create map
 column = []
 for i in range(19):
     column.append([])
-    # 바둑판 값 입력받음
+    # get map value
     column_input = input().split()
     for j in range(19):
         column[i].append(int(column_input[j]))
 
-# 총 개수와 x, y 좌표 입력받음
-# get total number, x, y
+# get total number and x, y location
 n = int(input())
 for i in range(n):
     x, y = input().split()
     x, y = int(x)-1, int(y)-1
-
+    
+# set x, y location(0 or 1) in the map
     for j in range(19):
         if column[x][j] != 0:
             column[x][j] = 0
@@ -25,6 +25,7 @@ for i in range(n):
         else:
             column[j][y] = 1
 
+# print map
 for i in range(19):
     for j in range(19):
         print(column[i][j], end=" ")
