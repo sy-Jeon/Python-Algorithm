162c5cd9f3d19aa83ba6946ae3c7a643979b919a
Sangyeol Jeon <ccccccccci@naver.com>
2021-09-24 15:13:14 +0900

    210924 Reverse the words.

---
+++ BaekjoonAlgorithm/BeginningLevel/9093.py
@@ -0,0 +1,18 @@
+'''
+Question: Reverse the words.
+
+info.
+t is the number of inputs.
+command is word.
+'''
+
+import sys
+
+t = int(input())
+
+for i in range(t):
+    command = sys.stdin.readline().split()
+    for j in range(len(command)):
+        command[j] = command[j][::-1]
+        print(command[j], end=" ")
+    print()
