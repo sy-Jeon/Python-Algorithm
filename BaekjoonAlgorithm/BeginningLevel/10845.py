976f83306550b877fe717e5d0030edb51456c8d5
Sangyeol Jeon <ccccccccci@naver.com>
2021-10-01 23:45:08 +0900

    211001 / Create a queue process program

--- BaekjoonAlgorithm/BeginningLevel/10845.py
+++ BaekjoonAlgorithm/BeginningLevel/10845.py
@@ -1,3 +1,16 @@
+'''
+Implement a queue that stores integers and then create a program that processes a given command as input.
+
+There are a total of 6 orders.
+
+Push X: Putting an integer X into a queue.
+pop: subtract the preceding integer from the queue and output the number. If there is no integer in the queue, output -1.
+size: Outputs an integer number in the queue.
+empty: Output 1 or 0 if the queue is empty.
+Front: Outputs an integer on the front of the queue. If there is no integer in the queue, output -1.
+Back: Outputs an integer at the end of the queue. If there is no integer in the queue, output -1.
+'''
+
 import sys
 
 n = int(input())
