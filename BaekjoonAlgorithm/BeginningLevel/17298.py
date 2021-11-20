fb6cdef6efdcfb0ad412611b663fc3d3c7e01d43
Sangyeol Jeon <ccccccccci@naver.com>
2021-11-21 01:28:17 +0900

    211110 / 오큰수

--- BaekjoonAlgorithm/BeginningLevel/17298.py
+++ BaekjoonAlgorithm/BeginningLevel/17298.py
@@ -5,7 +5,7 @@ stack = []
 answer = [-1] * num_count
 
 for i in range(num_count):
-    while stack and nums[stack[-1]] < nums[i]:
+    while stack and nums[stack[-1]] < nums[i] :
         answer[stack.pop()] = nums[i]
     stack.append(i)
 
