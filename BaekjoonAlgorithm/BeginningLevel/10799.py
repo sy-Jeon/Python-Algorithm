ed4e065751ea6c6fd499ad5325c7ae49f8858ce2
Sangyeol Jeon <ccccccccci@naver.com>
2021-11-21 01:32:42 +0900

    211025 / MissingInteger

--- BaekjoonAlgorithm/BeginningLevel/10799.py
+++ BaekjoonAlgorithm/BeginningLevel/10799.py
@@ -1,16 +1,15 @@
 bar_laser = list(input())
+
 answer = 0
 stack = []
 
 for i in range(len(bar_laser)):
     if bar_laser[i] == '(':
         stack.append('(')
-        
     else:
         if bar_laser[i-1] == '(':
             stack.pop()
             answer += len(stack)
-            
         else:
             stack.pop() 
             answer += 1
