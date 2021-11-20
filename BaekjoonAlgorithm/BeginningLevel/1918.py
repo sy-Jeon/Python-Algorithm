3a0f72bc992a6e9548b4f738bccc241c9a63841b
Sangyeol Jeon <ccccccccci@naver.com>
2021-11-21 00:14:40 +0900

    Update 1918.py

--- BaekjoonAlgorithm/BeginningLevel/1918.py
+++ BaekjoonAlgorithm/BeginningLevel/1918.py
@@ -9,8 +9,8 @@ for i in num:
         if i == '(':
             stack.append(i)
         elif i == '*' or i == '/':
-            # a+b*c 일 때 stack의 마지막 값이 + or -면 stack에는 *가 들어있지 않은 상태이기 때문에, answer에는 값이 [a, b, +]로 들어오게된다. 그렇기 때문에 *까지 스택에 쌓아야 answer에 값이 [a, b, c, *, +]로 들어오게 된다.
-            while stack and stack[-1] == '+' and stack[-1] == '-':
+            # A*B*C 일 때 Stack에 첫번 째 *이 쌓이고 두번 째 *일 때 stack에 있는 첫번 째 *을 answer에 붙인다.
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
                 answer += stack.pop()
             stack.append(i)
         elif i == '+' or i == '-':
