cac979396dd061ff6fb8e5c8a3c49058088674a9
Sangyeol Jeon <ccccccccci@naver.com>
2021-11-23 13:06:16 +0900

    211122 / The number of alphabet

--- BaekjoonAlgorithm/BeginningLevel/10808.py
+++ BaekjoonAlgorithm/BeginningLevel/10808.py
@@ -1,20 +1,33 @@
-c = ord("a")
-t = ord('z')
-alphabet = []
-count = 0
-
-while c <= t :
-    alphabet.append(chr(c))
-    c += 1
-    count += 1
-
-answer = [0] * count
-
+'''
+Modified code.
+'''
 s = input()
 
+answer = [0] * 26
+
 for i in s:
-    for j in range(len(alphabet)):
-        if i == alphabet[j]:
-            answer[j] += 1
+    answer[ord(i) - ord("a")] += 1
 
 print(*answer)
+
+
+# c = ord("a")
+# t = ord('z')
+# alphabet = []
+# count = 0
+
+# while c <= t :
+#     alphabet.append(chr(c))
+#     c += 1
+#     count += 1
+
+# answer = [0] * count
+
+# s = input()
+
+# for i in s:
+#     for j in range(len(alphabet)):
+#         if i == alphabet[j]:
+#             answer[j] += 1
+
+# print(*answer)
