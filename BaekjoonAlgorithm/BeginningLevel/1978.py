bc540ccb2883a6952cea1996c44b2590a0329296
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-02 23:58:53 +0900

    Update 1978.py

--- BaekjoonAlgorithm/BeginningLevel/1978.py
+++ BaekjoonAlgorithm/BeginningLevel/1978.py
@@ -1,15 +1,25 @@
-n = int(input())
-a = list(map(int, input().split()))
-count = 0
+x = int(input())
+n = list(map(int, input().split()))
+# count = 0
 
-for i in range(n):
+# for i in range(n):
 
-    for j in range(2, a[i]):
-        # print(a[i], "%", j)
+#     for j in range(2, x[i]):
 
-        if a[i] % j != 0:
-            print(a[i] % j)
-            count += 1
-            pass
+#         print(x[i] % j)
+#         if x[i] % j != 0:
+#             count += 1
 
-print(count)
+# print(count)
+
+def primeNumbers(x, n):
+    count = 0
+    for i in range(x):
+
+        for j in range(2, n[i]):
+            print(n[i] % j)
+            if n[i] % j == 0:
+                return print("소수 아니야",n[i], "%", j,"=", n[i] % j)
+        return print("소수 맞아")
+
+primeNumbers(x, n)
