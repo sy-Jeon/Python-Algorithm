0212da63dae80fc8f3a30764b52ebcdbfa20ea24
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-04 23:58:15 +0900

    Update 1929.py

--- BaekjoonAlgorithm/BeginningLevel/1929.py
+++ BaekjoonAlgorithm/BeginningLevel/1929.py
@@ -1,6 +1,20 @@
+m, n = map(int, input().split())
+
+for i in range(m, n+1):
+    if i > 1:
+        if i % 2 == 0:
+            if i == 2:
+                print(i)
+        elif i % 3 == 0:
+            if i == 3:
+                print(i)
+        else:
+            print(i)
+            
+
 '''
 Time over
-'''
+
 n = list(map(int, input().split()))
 
 seq = []
@@ -17,3 +31,4 @@ for i in range(n[0], n[1]+1):
         print(seq[index])
 
     index += 1
+'''
