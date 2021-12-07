33d6b1dcc6341c1d26e7688c51b07b356cfcd97f
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-07 21:31:53 +0900

    211205 / Searching the prime numbers 2

--- BaekjoonAlgorithm/BeginningLevel/1929.py
+++ BaekjoonAlgorithm/BeginningLevel/1929.py
@@ -3,9 +3,11 @@ m, n = map(int, input().split())
 for i in range(m, n+1):
     if i < 1:
         continue
+
     for j in range(2, int(i ** 0.5)+1):
         if i % j == 0:
             break
+
     else:
         print(i)
             
