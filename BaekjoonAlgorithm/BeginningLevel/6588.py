2b3b50733cb2d9a5729de5a71104b1f5b1bad68a
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-17 10:04:43 +0900

    211215 / Goldbach's Conjecture

--- BaekjoonAlgorithm/BeginningLevel/6588.py
+++ BaekjoonAlgorithm/BeginningLevel/6588.py
@@ -4,7 +4,6 @@ n 1742, Christian Goldbach, a German amateur mathematician, sent a letter to Leo
 Every even number greater than 4 can be written as the sum of two odd prime numbers.
 
 For example:
-
 8 = 3 + 5. Both 3 and 5 are odd prime numbers.
 20 = 3 + 17 = 7 + 13.
 42 = 5 + 37 = 11 + 31 = 13 + 29 = 19 + 23.
@@ -12,3 +11,4 @@ Today it is still unproven whether the conjecture is right. (Oh wait, I have the
 
 Anyway, your task is now to verify Goldbach's conjecture for all even numbers less than a million.
 '''
+
