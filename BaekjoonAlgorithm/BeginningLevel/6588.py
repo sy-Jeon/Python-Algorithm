1d2c37a2f7df779b24fd8eb87fab5d98d6a1f1ac
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-21 23:51:06 +0900

    Update 6588.py

--- BaekjoonAlgorithm/BeginningLevel/6588.py
+++ BaekjoonAlgorithm/BeginningLevel/6588.py
@@ -16,7 +16,7 @@ r= 1000000
 
 check = [True for _ in range(r)]
 
-for i in range(2,int(r**0.6)):
+for i in range(2,int(r**0.5)+1):
     if check[i]==True:
         for j in range(i*2, r, i) : 
             if check[j] == True :
