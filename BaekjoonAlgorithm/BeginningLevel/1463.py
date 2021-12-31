84ab4203c24244bc3db0153aba250e0c8d19ed3e
Sangyeol Jeon <ccccccccci@naver.com>
2021-12-31 17:21:21 +0900

    Update 1463.py

--- BaekjoonAlgorithm/BeginningLevel/1463.py
+++ BaekjoonAlgorithm/BeginningLevel/1463.py
@@ -1,1 +1,17 @@
+import sys
 
+read = sys.stdin.readline
+N = int(read())
+
+cache = [-1] * (N+3)
+cache[1] = 0
+cache[2] = cache[3] = 1
+
+for i in range(4, N+1):
+    cnt = 10**6
+    if i % 3 == 0:
+        cnt = min(cache[i//3], cnt)
+    if i % 2 == 0:
+        cnt = min(cache[i//2], cnt)
+    
+    cache[i] = min(cache[i-1], cnt) + 1
