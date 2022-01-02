d259684b3807bb76ec0b58069cd7fe7e51aa455c
Sangyeol Jeon <ccccccccci@naver.com>
2022-01-02 23:59:15 +0900

    Update 1463.py

--- BaekjoonAlgorithm/BeginningLevel/1463.py
+++ BaekjoonAlgorithm/BeginningLevel/1463.py
@@ -5,12 +5,14 @@ def solve():
   arr = [0, 0, 1, 1]
   for i in range(4, n+1):
     one, two, three = math.inf, math.inf, arr[i-1]
+    
     if i % 3 == 0:
       one = arr[i//3]
+      
     if i % 2 == 0:
       two = arr[i//2]
+      
     value = 1 + min(one, two, three)     
     arr.append(value)
-  print(arr[n])
  
 solve()
