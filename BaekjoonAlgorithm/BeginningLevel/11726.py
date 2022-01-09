c1ea807d295c8c9ff4bb58c26016e43c4878f0a2
Sangyeol Jeon <ccccccccci@naver.com>
2022-01-10 00:55:53 +0900

    220109 / 2 x n tiling

--- BaekjoonAlgorithm/BeginningLevel/11726.py
+++ BaekjoonAlgorithm/BeginningLevel/11726.py
@@ -1,8 +1,7 @@
-s = [0, 1, 2]
-
-for i in range(3, 1001):
-  s.append(s[i - 2] + s[i - 1])
- 
 n = int(input())
+arr = [0, 1, 2]
 
-print(s[n] % 10007)
+for i in range(3, n+1):
+    arr.append(arr[i-1] + arr[i-2])
+    
+print(arr[n] % 10007)
