221fe71e2fec61c3b89078514c7b700aae1fd191
Sangyeol Jeon <ccccccccci@naver.com>
2022-01-15 23:50:31 +0900

    Update 9095.py

--- BaekjoonAlgorithm/BeginningLevel/9095.py
+++ BaekjoonAlgorithm/BeginningLevel/9095.py
@@ -1,4 +1,7 @@
-total_n = int(input())
-
-for i in range(total_n):
-    print(i)
+t = int(input())
+ott = [1, 2, 4]
+for i in range(3, 10):
+    ott.append(ott[i - 3] + ott[i - 2] + ott[i - 1])
+for i in range(t):
+    n = int(input())
+    print(ott[n - 1])
