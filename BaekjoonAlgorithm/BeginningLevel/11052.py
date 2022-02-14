b57a27b4481c478d6b20a3a2f54edbf0c4022f06
Sangyeol Jeon <ccccccccci@naver.com>
2022-02-14 20:58:05 +0900

    Update 11052.py

--- BaekjoonAlgorithm/BeginningLevel/11052.py
+++ BaekjoonAlgorithm/BeginningLevel/11052.py
@@ -1,9 +1,1 @@
-n = int(input())
-d = [0] * (n + 1)
-p = [0] + list(map(int, input().split()))
-d[1] = p[1]
-for i in range(2, n + 1):
-    for j in range(1, i + 1):
-        if d[i] < d[i - j] + p[j]:
-            d[i] = d[i - j] + p[j]
-print(d[n])
+
